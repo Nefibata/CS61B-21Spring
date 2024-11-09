689856651177228fad6aac6917e5db67f1e45d79
zxk <3136605808@qq.com>
2024-11-09 21:48:18 +0800

    temp

--- proj1/deque/LinkedListDeque.java
+++ proj1/deque/LinkedListDeque.java
@@ -1,2 +1,26 @@
-package deque;public class LinkedListDeque {
+package deque;
+
+public class LinkedListDeque <T> {
+    private int size;
+    private List<T> head;
+    private List<T> tail;
+    public LinkedListDeque (){
+        this.head=new List<T>(null,null,null);
+        this.tail=head;
+        size=0;
+    }
+    public void addFirst(T item){
+        List<T> temp = new List<>(item,)
+    }
+    public void addLast(T item){
+
+    }
+    public boolean isEmpty(){
+
+    }
+    public int size(){
+
+    }
+
+
 }
--- proj1/deque/List.java
+++ proj1/deque/List.java
@@ -1,2 +1,36 @@
-package deque;public class List {
+package deque;
+
+public class List <T> {
+    private T value;
+    private List<T> pr;
+    private List<T> next;
+    public List(T value,List<T> pr,List<T> next){
+        this.next=next;
+        this.pr=pr;
+        this.value=value;
+    }
+
+    public List<T> getNext() {
+        return next;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public List<T> getPr() {
+        return pr;
+    }
+
+    public void setNext(List<T> next) {
+        this.next = next;
+    }
+
+    public void setPr(List<T> pr) {
+        this.pr = pr;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
 }
